70a72948206dacb3e74b3ec9aa786923eaec07d3
asimolmez <asimolmez@outlook.com>
2019-07-05 15:16:57 +0300

    test env initialized

--- src/NesButton.js
+++ src/NesButton.js
@@ -1,11 +1,35 @@
-import {Text} from "react-native";
+// Intro.js
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-const NesButton = ( ) => {
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+});
+
+export default class Intro extends Component {
+  render() {
     return (
-        <Text style={{fontSize: 50}}>
-            HELLO WORLD!
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>
+          This is a React Native snapshot test.
         </Text>
-    )
+      </View>
+    );
+  }
 }
-
-export default NesButton;
